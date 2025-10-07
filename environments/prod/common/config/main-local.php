272a80b53f5c3f99004662098a2199cd2bd75e7d
quar15 <quar5@wp.pl>
2025-10-07 17:12:56 +0200

    chore: cleanup prod config, simplify login screen

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -4,10 +4,15 @@ return [
     'components' => [
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
+            'dsn' => 'pgsql:host=localhost;dbname=yiivian',
+            'username' => 'yiivian',
             'password' => '',
             'charset' => 'utf8',
+            'enableQueryCache' => true,
+            'queryCacheDuration' => 5,
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 30,
+            'schemaCache' => 'cache',
         ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
